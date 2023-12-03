9f7faf883d9c3581da55c7399cddbc6e1157f6f0
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2023-12-03 02:03:32 -0900

    inital app

---
+++ lib/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class HomeSCREEN extends StatelessWidget {
+   HomeSCREEN({Key? key}) : super(key: key);
+var locationManger=Location();
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
