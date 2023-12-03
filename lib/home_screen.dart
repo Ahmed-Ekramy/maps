1c155992659dd89b7cb5e5e8562e045ead118c3e
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2023-12-03 02:20:38 -0900

    four functions

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,5 +7,22 @@ var locationManger=Location();
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
+
   }
+Future<bool> isLocationServiceEnable()async{
+ return  await  locationManger.serviceEnabled();
+}
+Future<bool> requestService()async{
+ var enable=await  locationManger.requestService();
+ return enable;
+}
+Future<bool> isPermissionGranted()async{
+ var permissionStatus=await  locationManger.hasPermission();
+ return permissionStatus==PermissionStatus.granted;
+}
+Future<bool> requestPermission()async{
+ var permissionStatus=await  locationManger.requestPermission();
+ return permissionStatus==PermissionStatus.granted;
+}
+
 }
